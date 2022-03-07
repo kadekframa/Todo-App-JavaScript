3b84e10e9ede485b3dc98df1491ef338cac31526
kadekframa <dframa19@gmail.com>
2022-03-07 20:35:10 +0800

    Practice Todo App JavaScript. Menampilkan item TODO yang sudah selesai dan Membuat fitur hapus TODO. Keep Spirit!!! dan Tetap Semangat !!!

--- js/script.js
+++ js/script.js
@@ -39,11 +39,16 @@ document.addEventListener(RENDER_EVENT, function(){
     const uncompletedTODOList = document.getElementById("todos");
     uncompletedTODOList.innerHTML = "";
 
+    const completedTODOList = document.getElementById("completed-todos");
+    completedTODOList.innerHTML = "";
+
     for (let todoItem of todos) {
         const todoElement = makeTodo(todoItem);
 
         if(todoItem.isCompleted == false){
             uncompletedTODOList.append(todoElement);
+        }else{
+            completedTODOList.append(todoElement);
         }
     }
 
@@ -111,4 +116,33 @@ function findTodo(todoId){
     }
 
     return null;
+}
+
+
+function removeTaskFromCompleted(todoId){
+    const todoTarget = findTodoIndex(todoId);
+    if(todoTarget === -1) return;
+    todos.splice(todoTarget, 1);
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+
+function undoTaskFromCompleted(todoId){
+    const todoTarget = findTodo(todoId);
+    if(todoTarget == null) return;
+
+    todoTarget.isCompleted = false;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+
+function findTodoIndex(todoId){
+    for(index in todos){
+        if(todos[index].id === todoId){
+            return index;
+        }
+    }
+
+    return -1;
 }
