43fcc7bf6b17d0d16c41365a78fc394e1869b7c9
kadekframa <dframa19@gmail.com>
2022-03-11 01:21:42 +0800

    Learning by doing Web Storage. Mengimplementasikan Web Storage ke Projek TODO Apps. Tetap Semangat Berlatih!!!, Pantang Menyerah !!!

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ function addTodo(){
     todos.push(todoObject);
 
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function generatedId(){
@@ -106,6 +107,7 @@ function addTaskToCompleted(todoId){
 
     todoTarget.isCompleted = true;
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function findTodo(todoId){
@@ -125,6 +127,7 @@ function removeTaskFromCompleted(todoId){
     todos.splice(todoTarget, 1);
 
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 
@@ -134,6 +137,7 @@ function undoTaskFromCompleted(todoId){
 
     todoTarget.isCompleted = false;
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 
@@ -146,3 +150,28 @@ function findTodoIndex(todoId){
 
     return -1;
 }
+
+
+// function saveData.
+function saveData(){
+    if(isStorageExist()){
+        const parsed = JSON.stringify(todos);
+        localStorage.setItem(STORAGE_KEY, parsed);
+        document.dispatchEvent(new Event(SAVED_EVENT));
+    }
+}
+
+const SAVED_EVENT = "saved-todo";
+const STORAGE_KEY = "TODO_APPS";
+
+function isStorageExist(){  /* boolean */
+    if(typeof(Storage) === "undefined"){
+        alert("Browser yang Anda gunakan belum mendukung fitur Web Storage");
+        return false;
+    }
+    return true;
+}
+
+document.addEventListener(SAVED_EVENT, function(){
+    console.info(localStorage.getItem(STORAGE_KEY));
+});
